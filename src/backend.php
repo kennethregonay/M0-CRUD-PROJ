83fdbcca71f4ba517d0a9326feb7331ccd318357
Netkee <kenneth.regonay@gmail.com>
2021-01-05 16:14:22 +0800

    create a backend functionalities

---
+++ src/backend.php
@@ -0,0 +1,28 @@
+<?php
+    $connection = new mysqli('localhost','root', '' , 'hris') 
+    or die(mysqli_error($connection));
+
+    if (isset($_POST['create'])){
+        $fname = $_POST ['fname'];
+        $mname = $_POST ['mname']; 
+        $lname = $_POST ['lname']; 
+        $address = $_POST ['address']; 
+        if($_POST ['gender']== 'male'){
+            $gender = 'M'; 
+        }else{
+            $gender = 'F'; 
+        }
+       
+        $bdate = $_POST ['bdate']; 
+        $poscode = $_POST ['poscode'];  
+        $connection->query("INSERT INTO employee(Firstname,Middlename,Lastname,Address,Gender,Birthdate,Position_Code) 
+        VALUES ('$fname','$mname','$lname','$address','$gender','$bdate','$poscode')")
+        or die ($connection->error);
+       }
+      
+    function prompt ($class){
+        $element = "<h6 class = '$class'>Successfully Added</h6>";
+    }
+       mysqli_close($connection);
+
+?> 
